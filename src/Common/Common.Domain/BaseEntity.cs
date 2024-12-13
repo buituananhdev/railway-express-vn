80dd0e4e1e227d6b208a9130aed4e7f5d8c2c60b
rashford_bui <rashford.bui@avepoint.com>
2024-12-13 14:48:01 +0700

    feat: apply repository and uow pattern :star:

---
+++ src/Common/Common.Domain/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Common.Domain
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ src/Common/Common.Infrastructure/IDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Common.Infrastructure
+{
+    public interface IDataContext : IDisposable
+    {
+        DbSet<T> Set<T>() where T : class;
+        DatabaseFacade Database { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
