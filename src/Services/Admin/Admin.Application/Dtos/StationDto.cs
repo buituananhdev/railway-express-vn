402093f4bad5f1423cc6d7fe3e4218ab42b641f9
rashford_bui <rashford.bui@avepoint.com>
2024-12-16 17:17:24 +0700

    feat: add station and get stations api :sparkles:

---
+++ src/Services/Admin/Admin.Application/Dtos/StationDto.cs
@@ -0,0 +1,12 @@
+﻿using Common.Domain;
+
+namespace Admin.Application.Dtos
+{
+    public class StationDto : BaseEntity
+    {
+        public string StationName { get; set; }
+        public string Location { get; set; }
+        public int StationOrder { get; set; }
+        public ICollection<TrainStatusDto>? TrainAtStation { get; set; }
+    }
+}
---
+++ src/Services/Admin/Admin.Application/Services/StationService/StationService.cs
@@ -0,0 +1,35 @@
+﻿using Admin.Application.Dtos;
+using Admin.Application.Repositories;
+using Admin.Domain.Entities;
+using AutoMapper;
+using System.Linq.Expressions;
+
+namespace Admin.Application.Services.StationService
+{
+    public class StationService : IStationService
+    {
+        private readonly IAdminUnitOfWork _adminUnitOfWork;
+        private readonly IMapper _mapper;
+
+        public StationService(IAdminUnitOfWork adminUnitOfWork, IMapper mapper)
+        {
+            _adminUnitOfWork = adminUnitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task AddStationAsync(AddStationDto addStationDto)
+        {
+            var station = _mapper.Map<Station>(addStationDto);
+            await _adminUnitOfWork.StationRepository.AddAsync(station);
+            await _adminUnitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<List<StationDto>> GetStations()
+        {
+            var stations = await _adminUnitOfWork.StationRepository
+                .ToListAsync(includes: new List<Expression<Func<Station, object>>> { station => station.TrainAtStation! });
+
+            return _mapper.Map<List<StationDto>>(stations);
+        }
+    }
+}
