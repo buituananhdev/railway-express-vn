ba89b95d09d8bd83ea450d922daa3c0a9ac4e5fe
rashford_bui <rashford.bui@avepoint.com>
2024-12-16 13:16:03 +0700

    refactor: api version tag and refactor structure passenger service :hammer:

--- src/Services/Passenger/Passenger.Application/IPassengerRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Common.Application.Repositories;
-
-namespace Passenger.Application
-{
-    public interface IPassengerRepository : IBaseRepository<Passenger.Domain.Entities.Passenger>
-    {
-    }
-}
