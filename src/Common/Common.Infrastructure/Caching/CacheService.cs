2a176860c5b5000e6ee99b733c1b92dd2d8c60b2
rashford_bui <rashford.bui@avepoint.com>
2025-02-25 13:54:29 +0700

    feat: lock seats api :sparkles:

--- src/Common/Common.Infrastructure/Caching/CacheService.cs
+++ src/Common/Common.Infrastructure/Caching/CacheService.cs
@@ -38,4 +38,24 @@ public class CacheService : ICacheService
 
         return _distributedCache.SetStringAsync(key, JsonSerializer.Serialize(value), options);
     }
+    public async Task<bool> ExistsAsync(string key)
+    {
+        var data = await _distributedCache.GetStringAsync(key);
+        return data != null;
+    }
+
+    public async Task SetMultipleAsync(List<KeyValuePair<string, string>> entries, TimeSpan expiry)
+    {
+        var tasks = entries.Select(entry => 
+            _distributedCache.SetStringAsync(entry.Key, entry.Value, new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expiry
+            }));
+        await Task.WhenAll(tasks);
+    }
+    public async Task<bool[]> ExistsMultipleAsync(string[] keys)
+    {
+        var tasks = keys.Select(async key => await _distributedCache.GetStringAsync(key) != null);
+        return await Task.WhenAll(tasks);
+    }
 }
