1654a89462b504fd27a74810f4323bfcae95e7aa
TuanAnh1003 <anhaanh2003@gmail.com>
2025-02-28 15:57:16 +0700

    fix: change seat number and traincar number to integer :bug:

--- src/Services/Admin/Admin.Application/Dtos/SeatDto.cs
+++ src/Services/Admin/Admin.Application/Dtos/SeatDto.cs
@@ -5,6 +5,6 @@ namespace Admin.Application.Dtos;
 public class SeatDto : BaseDto
 {
     public Guid TrainCarId { get; set; }
-    public string SeatNumber { get; set; }
+    public int SeatNumber { get; set; }
     public SeatStatusEnum Status { get; set; }
 }
---
+++ src/Services/Admin/Admin.Infrastructure/Persistence/Migrations/20250228062221_UpdateSeatNumberAndCarNumber.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Admin.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeatNumberAndCarNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CarNumber",
+                table: "TrainCars",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SeatNumber",
+                table: "Seats",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CarNumber",
+                table: "TrainCars",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SeatNumber",
+                table: "Seats",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
