48aad1d0b609c84539c74adc34972043c8a3e846
vancuong1808 <vancuong1808@gmail.com>
2025-03-24 22:02:00 +0700

    feat: add api crud, pagination passenger and useraccount

---
+++ src/Services/UserManagement/UserManagement.Application/Dtos/PaginatePassengerDto.cs
@@ -0,0 +1,11 @@
+﻿using Common.Domain;
+
+namespace UserManagement.Application.Dtos;
+public class PaginatePassengerDto : BaseEntity
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public RoleEnum Role { get; set; }
+}
---
+++ src/Services/UserManagement/UserManagement.Application/Dtos/UpdatePassengerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagement.Application.Dtos;
+public class UpdatePassengerDto
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string NewPassword { get; set; }
+    public string PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public string Role { get; set; }
+}
