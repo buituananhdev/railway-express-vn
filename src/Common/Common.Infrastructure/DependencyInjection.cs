4e6fe27f2dbda39ddb9af4b401ed881adf03e0ac
TuanAnh1003 <anhaanh2003@gmail.com>
2025-05-11 12:47:18 +0700

    Add elk logging

--- src/Common/Common.Infrastructure/DependencyInjection.cs
+++ src/Common/Common.Infrastructure/DependencyInjection.cs
@@ -1,13 +1,8 @@
 ﻿using Common.Application.Interfaces;
-using Common.Application.Repositories;
 using Common.Infrastructure.Caching;
 using Common.Infrastructure.Logging;
-using Common.Infrastructure.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Serilog.Sinks.Elasticsearch;
-using Serilog;
 using StackExchange.Redis;
 
 namespace Common.Infrastructure;
@@ -33,22 +28,6 @@ public static class DependencyInjection
         services.AddScoped<ICacheService, CacheService>();
         services.AddScoped<IDistributedLockService, RedisLockService>();
 
-
-        //Log.Logger = new LoggerConfiguration()
-        //        .Enrich.FromLogContext()
-        //        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticSearch:Uri"]))
-        //        {
-        //            AutoRegisterTemplate = true,
-        //            IndexFormat = $"{configuration["ElasticSearch:DefaultIndex"]}-{DateTime.UtcNow:yyyy.MM.dd}",
-        //        })
-        //        .CreateLogger();
-
         return services;
-    }
-
-    public static IHostBuilder UseSerilogLogging(this IHostBuilder hostBuilder)
-    {
-        hostBuilder.UseSerilog();
-        return hostBuilder;
     }
 }
