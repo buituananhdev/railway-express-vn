9fff3b192d6de35e05848fff8390f17967e86cfb
TuanAnh1003 <anhaanh2003@gmail.com>
2025-05-27 00:38:32 +0700

    perf: get traincars and seats api optimize performance

--- src/Services/Admin/Admin.Application/Dtos/GetTrainSchedulesDto.cs
+++ src/Services/Admin/Admin.Application/Dtos/GetTrainSchedulesDto.cs
@@ -1,8 +1,9 @@
 ﻿namespace Admin.Application.Dtos;
+
 public class GetTrainSchedulesDto
 {
     public Guid DepartureStationId { get; set; }
     public Guid ArrivalStationId { get; set; }
     public DateTime DepartureDate { get; set; }
-    public DateTime? ReturnDate { get; set; } = null;
+    public DateTime? ReturnDate { get; set; }
 }
